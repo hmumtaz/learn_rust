8a1b4ba048e55b618c58716fa5781a8ac54fe717
Hussain Mumtaz <hmumtaz996@gmail.com>
2022-07-16 22:54:18 -0700

    Update minigrep to use closures and iterators

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -9,12 +9,17 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough args");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+    pub fn new(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        // ignore first argument
+        args.next();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("No Query String Provided"),
+        };
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("No File Name Provided"),
+        };
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
         return Ok(Config {
             query,
@@ -39,24 +44,17 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut res: Vec<&str> = vec![];
-    for line in contents.lines() {
-        if line.contains(&query) {
-            res.push(line);
-        }
-    }
-    res
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut res: Vec<&str> = vec![];
-    let query = query.to_lowercase();
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            res.push(line);
-        }
-    }
-    res
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(query))
+        .collect()
 }
 
 #[cfg(test)]
